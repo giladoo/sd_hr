6067d45e1a68d993961de9895808976a7b21fd69
arash <homayounfar.arash@gmail.com>
2025-02-05 14:36:38 +0330

    init 18.0

--- __manifest__.py
+++ __manifest__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 {
     'name': 'SD HR',
-    'version': '17.0.1.0.0',
+    'version': '18.0.1.0.0',
     'category': 'Human Resources',
     'summary': """ It works as base module for HR extended modules """,
     'author': 'Arash Homayounfar',

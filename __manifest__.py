cd4bd02ad10ba0d0f25e016646df8415456bfb73
arash <homayounfar.arash@gmail.com>
2025-02-19 02:37:01 +0330

    exportList

--- __manifest__.py
+++ __manifest__.py
@@ -20,6 +20,7 @@
         'views/hr_employee_views.xml',
         'views/variables_views.xml',
         'views/views.xml',
+        'report/export_list.xml',
         'data/barcode_sequence.xml',
     ],
     'assets':{
